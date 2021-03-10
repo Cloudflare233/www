1195dffe17795f196ae45a6b9715b0d42a36de11
Andrew <xsxg123456@gmail.com>
2021-03-10 19:08:52 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
-import { Page, Text ,Card } from '@geist-ui/react'
+import { Page, Text ,Card ,Button,Link } from '@geist-ui/react'
+
+import { CssBaseline, GeistProvider } from '@geist-ui/react'
+
+const Dark = () => {
+  const [themeType, setThemeType] = useState('light')
+  const switchThemes = () => {
+    setThemeType(last => (last === 'dark' ? 'light' : 'dark'))
+  }
+  return (
+    <GeistProvider themeType={themeType}>
+      <CssBaseline />
+      <Link onClick={switchThemes}>Dark Mode</Link>
+    </GeistProvider>
+  )
+}
 
 export default function Home() {
   return (
  <Page dotBackdrop size="mini">
 <Card shadow>
       <Page.Header>
-        <Text h2>React Application with Geist UI</Text>
+        <Text h2>Andrew</Text>
       </Page.Header>
       <Text>
-        Hello, I am using <Text b>Gesit UI</Text> !
+        Hello, I am using <Text b>Gesit UI</Text>.See the <Dark />!
       </Text>
 </Card>
     </Page>
