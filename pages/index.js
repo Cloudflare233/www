a80fdb23b3a44043790decf7321a329b03ab67e2
Andrew <xsxg123456@gmail.com>
2021-03-10 19:02:11 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import { Page, Text } from '@geist-ui/react'
+import { Page, Text ,Card } from '@geist-ui/react'
 
 export default function Home() {
   return (
-    <Page dotBackdrop size="mini">
+ <Page dotBackdrop size="mini">
+<Card shadow>
       <Page.Header>
         <Text h2>React Application with Geist UI</Text>
       </Page.Header>
       <Text>
         Hello, I am using <Text b>Gesit UI</Text> !
       </Text>
+</Card>
     </Page>
   )
 }
